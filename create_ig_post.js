0b513282b254353e3ee75de51af878893d6078c0
apuente93 <alejandropuentetelle@gmail.com>
2023-01-24 16:11:19 -0700

    Created script 2: authenticates with Instagram API and posts image to account

---
+++ create_ig_post.js
@@ -0,0 +1,30 @@
+const Facebook = require('facebook-node-sdk');
+
+const facebook = new Facebook({
+    appId: '564541868598695',
+    secret: 'b9c161dc6aa25357ece9699382ab3114'
+});
+
+/*
+facebook.api('/10167233336530707/accounts?access_token=EAAH3lJRepkQBAHTyqGSrgqKf4GL7uuprQClkCf7bhc23FZBWx9ZBE0YNH3fJ0rkm4waUZCKk56r7U1deDtZBnjQnDvA6IXZCnqlUfphONkFNtZAZCUg07EfyaInhfynNFZCr6cupRHVbEL9hMWRjgTghIXnp55BFSPzNCPLrQgfQ6SXyZBZCAAjaBVZC1NyM43mSegroLHiGBZAY2l0eXk3XjtZCA', (res) => {
+    if(!res || res.error) {
+        console.log(!res ? 'error occurred' : res.error);
+        return;
+    }
+    console.log('Instagram ID: ' + res);
+});
+*/
+
+facebook.api(`/17841456553385778/media`, 'POST', {
+    caption: 'Peacocks',
+    image_url: 'https://oaidalleapiprodscus.blob.core.windows.net/private/org-ys3ClCSRPTqgHq1xvRUulECW/user-cugpfcbMKkMSoCUvaIn0QpE8/img-seMaaQ6ei62q562aYEko9nnd.png?st=2023-01-15T01%3A47%3A17Z&se=2023-01-15T03%3A47%3A17Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-01-14T23%3A59%3A59Z&ske=2023-01-15T23%3A59%3A59Z&sks=b&skv=2021-08-06&sig=Dqey%2BHSLGx5XD3smBXvy2krY36sszudWbdI22QU%2BCdk%3D',
+    access_token: 'EAAIBcqWJ5acBAM26TNdQ5JBe03oSXZCja4x6ahPNsHJ1vWZCJa7We6xd34PxB0cNCIZAzJgdfXL1pBmbl3WT5kcr2yRZB7Q6vQwbw09U8x2ONJtbbnUi5Mkej8HbuIKgtnslqUJwQm5FPEZCRGmROvqUz64E0GVtApXxEWVOnpIoJzgpAZAE4TzCTuycWt2QNGs7NENK621VXim7GR5gHf'
+}, (res) => {
+    if(!res || res.error) {
+        console.log(!res ? 'error occurred' : res.error);
+        return;
+    }
+    console.log('Post Id: ' + res);
+});
+
+
--- image_generation.js
+++ image_generation.js
@@ -10,7 +10,7 @@ const openai = new OpenAIApi(config);
 
 async function generatePrompt() {
     const response = await openai.createCompletion({
-        prompt: "Generate a prompt for an AI-generated image of a beautiful natural occurance in the world. Make sure it is a different random occurance every time I ask you again. Here are 2 examples: 1. A beautiful sunset in a beach in the carribean. 2. A bee polinating a plant in madrid, spain.",//'Generate a prompt for an AI-generated image of a famous person, doing something funny. Make sure the prompt output is a complete sentence that makes sense. You must generate a random famous person and a random action every prompt.',
+        prompt: "Generate a prompt for an AI-generated image of a beautiful natural occurance in the world including an animal or many animals. Make sure it is a different random occurance every time I ask you again. Here are 2 examples of prompts that could be generated: 1. A pack of exotic sharks, swimming together in the deep blue sea. 2. A bee polinating a plant, about to be eaten by a bear.",//'Generate a prompt for an AI-generated image of a famous person, doing something funny. Make sure the prompt output is a complete sentence that makes sense. You must generate a random famous person and a random action every prompt.',
         model: "text-davinci-003"
     });
     console.log(response.data.choices[0].text);
@@ -20,7 +20,7 @@ async function generatePrompt() {
 
 async function generateImage() {
     const response = await openai.createImage({
-        prompt: await generatePrompt(),
+        prompt: "Create a logo for an ai generating instagram account that generates images of beautiful natural occurances that happen in the world.",//await generatePrompt(),
 		n: 1,
 		size: "1024x1024",
     });
